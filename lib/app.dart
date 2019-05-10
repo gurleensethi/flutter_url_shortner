3aa62abd9181fcd6e54f9d6af13fa33e503e19a2
Gurleen Sethi <sarusethi@rocketmail.com>
2019-05-10 15:15:20 -0400

    Initial Commit

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_url_shortner/ui/home.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
